d3ad0d94d1c424a3fa685e9c90f4a64e73c4ad17
Vladimir Velikov <115097684+thevlad0@users.noreply.github.com>
2023-11-05 22:02:47 +0200

    Update func.cpp

--- Game/func.cpp
+++ Game/func.cpp
@@ -1,18 +1,3 @@
-/**
-*  
-* Solution to course project # 7
-* Introduction to programming course
-* Faculty of Mathematics and Informatics of Sofia University
-* Winter semester 2022/2023
-*
-* @author Vladimir Velikov
-* @idnumber 0MI0600204
-* @compiler VS
-*
-* <file with main functionalities of the game>
-*
-*/
-
 #include "func.h"
 #include <iostream>
 
